fe9dd9c6c089317790cfb85992a4006bb3e81f1f
Jonathan Hernández <ion@suavizado.com>
2012-04-10 19:19:14 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitacora::Application.config.secret_token = 'fdc9c9595fc56f58c3dddd0c11e695d311d7b00b3ff364ee722e757ccdbcd727bb84e90ed3fbf796e569c0d66ae0284f93087436d0bfeb2711568b072034f2c7'

c96d0728c3186f5b8175a4274bb392185a6a2424
Jonathan Hernandez <ion@suavizado.com>
2012-05-10 15:49:53 -0600

    File uploader

---
+++ app/models/service_file.rb
@@ -0,0 +1,12 @@
+class ServiceFile < ActiveRecord::Base
+  attr_accessible :file, :description, :user_id, :service_request_id, :sample_id, :requested_service_id, :file_type_id
+
+  mount_uploader :file, ServiceFileUploader
+  validates :description, :presence => true
+
+  before_destroy :delete_linked_file
+
+  def delete_linked_file
+    self.remove_file!
+  end
+end
---
+++ db/migrate/20120510214010_create_service_files.rb
@@ -0,0 +1,18 @@
+class CreateServiceFiles < ActiveRecord::Migration
+  def change
+    create_table :service_files do |t|
+      t.references :user
+      t.references :service_request
+      t.references :sample
+      t.references :requested_service
+      t.string     :file_type
+      t.string     :description
+      t.string     :file
+      t.timestamps
+    end
+    add_index('service_files', 'user_id')
+    add_index('service_files', 'service_request_id')
+    add_index('service_files', 'sample_id')
+    add_index('service_files', 'requested_service_id')
+  end
+end

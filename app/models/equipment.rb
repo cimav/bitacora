1fbee3a9ae749e38480995e21ddb35756ee788ff
Jonathan Hernandez <ion@suavizado.com>
2015-05-11 17:51:21 -0600

    Agregar campos

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,5 @@
 class Equipment < ActiveRecord::Base
-  attr_accessible :laboratory_id, :name, :description, :hourly_rate, :status, :item_number
+  attr_accessible :laboratory_id, :name, :description, :hourly_rate, :status, :item_number, :budget_item, :purchase_date, :purchase_price, :internal_hourly_rate
 
   belongs_to :laboratory
   

ae1a08f9a7e7288c935b1b799501f01c67807c12
Jonathan Hernandez <ion@suavizado.com>
2017-06-27 17:50:35 -0600

    MOdificar nombre para quitarle simbolos

--- app/uploaders/vinculacion_file_uploader.rb
+++ app/uploaders/vinculacion_file_uploader.rb
@@ -1,6 +1,11 @@
 # encoding: utf-8
 
 class VinculacionFileUploader < CarrierWave::Uploader::Base
+
+  def filename
+    original_filename.gsub(/ /i,'_').gsub(/[^\.a-z0-9_]/i, '') if original_filename
+  end
+
   def store_dir
     "#{Rails.root}/../sigre/private/solicitud/#{model.solicitud_id}/archivos/"
   end
